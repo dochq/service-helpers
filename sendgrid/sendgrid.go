4eccf8cb34fc73f678ce7f3c7299aaab92619285
Oleg Vakheta <oleg.vakheta@dochq.co.uk>
2022-04-13 17:04:32 +0300

    add sendgrid func

---
+++ sendgrid/sendgrid.go
@@ -0,0 +1,80 @@
+package sendgrid
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/sendgrid/rest"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+var sendgridClient *sendgrid.Client
+
+type FileInfo struct {
+	Name   string
+	Type   string
+	Buffer []byte
+}
+
+func InitSendgrid(dsn, key string) error {
+	clientOptions := sentry.ClientOptions(sentry.ClientOptions{
+		Dsn:              dsn,
+		AttachStacktrace: true,
+	})
+	if err := sentry.Init(clientOptions); err != nil {
+		return err
+	}
+	sendgridClient = sendgrid.NewSendClient(key)
+	return nil
+}
+
+func SendGridEmail(sendGridEmailTmpl string, fromEmail *mail.Email, receipients map[string][]*mail.Email, subject, body string, files []*FileInfo) (*rest.Response, error) {
+	var peopleToEmail []*mail.Email
+
+	for _, receipient := range receipients["to"] {
+		peopleToEmail = append(peopleToEmail, &mail.Email{
+			Name:    receipient.Name,
+			Address: receipient.Address,
+		})
+	}
+
+	sendData := &mail.SGMailV3{
+		TemplateID: sendGridEmailTmpl,
+		Subject:    subject,
+		From: &mail.Email{
+			Name:    fromEmail.Name,
+			Address: fromEmail.Address,
+		},
+		Personalizations: []*mail.Personalization{
+			{
+				To: peopleToEmail,
+				DynamicTemplateData: map[string]interface{}{
+					"email_subject": subject,
+					"email_body":    body,
+				},
+			},
+		},
+	}
+
+	for _, file := range files {
+		sendData.AddAttachment(&mail.Attachment{
+			Content:     base64.StdEncoding.EncodeToString(file.Buffer),
+			Type:        file.Type,
+			Filename:    file.Name,
+			Disposition: "attachment",
+		})
+	}
+
+	res, err := sendgridClient.Send(sendData)
+	if err != nil {
+		return res, fmt.Errorf("sendgridClient.Send: %s", err)
+	}
+
+	if res.StatusCode != 202 {
+		return res, fmt.Errorf("sendgridClient.Send: incorrect status code returned: %v, %s", res.StatusCode, res.Body)
+	}
+
+	return res, nil
+}
