ce24977f177f3b246f8a4b6d5176e57d65014607
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-01-26 18:11:41 +0200

    INFS-67 try fix "read: connection reset by peer"

--- go.mod
+++ go.mod
@@ -2,18 +2,13 @@ module github.com/dochq/service-helpers
 
 go 1.21
 
-require (
-	github.com/sendgrid/rest v2.6.9+incompatible
-	github.com/sendgrid/sendgrid-go v3.12.0+incompatible
-	google.golang.org/grpc v1.60.1
-)
+require google.golang.org/grpc v1.61.0
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- grpc/grpc.go
+++ grpc/grpc.go
@@ -12,12 +12,6 @@ import (
 type contextKey string
 
 const (
-	// Time  - Keepalive Server Parameter
-	Time = 5 * time.Second
-	// Timeout  - Keepalive Server Parameter
-	Timeout = 5 * time.Second
-	// MaxConnectionAgeGrace  - Keepalive Server Parameter
-	MaxConnectionAgeGrace = 10 * time.Second
 	// EmptyContextKey - empty context key
 	EmptyContextKey = contextKey("")
 )
@@ -27,14 +21,22 @@ var ServerKeepaliveParams = grpc.KeepaliveParams(keepalive.ServerParameters{
 	// After a duration of this time if the server doesn't see any activity it
 	// pings the client to see if the transport is still alive.
 	// If set below 1s, a minimum value of 1s will be used instead.
-	Time: Time,
+	// Set to a relatively short duration to detect idle connections faster.
+	Time: 30 * time.Second,
+
 	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
-	Timeout: Timeout,
+	// Set to a value higher than Time to allow for potential network delays.
+	Timeout: 60 * time.Second,
+
 	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
-	MaxConnectionAgeGrace: MaxConnectionAgeGrace,
+	// Set conservatively to provide extra time before forcibly closing.
+	MaxConnectionAgeGrace: 60 * time.Second,
+
+	// Set to a value that accommodates your application's requirements.
+	MaxConnectionAge: 5 * time.Minute,
 })
 
 // ClientKeepaliveParams - gRPC Client Keepalive Parameters
@@ -42,11 +44,11 @@ var ClientKeepaliveParams = grpc.WithKeepaliveParams(keepalive.ClientParameters{
 	// After a duration of this time if the client doesn't see any activity it
 	// pings the server to see if the transport is still alive.
 	// If set below 10s, a minimum value of 10s will be used instead.
-	Time: Time,
+	Time: 30 * time.Second,
 	// After having pinged for keepalive check, the client waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
-	Timeout: Timeout,
+	Timeout: 60 * time.Second,
 	// If true, client sends keepalive pings even with no active RPCs. If false,
 	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
